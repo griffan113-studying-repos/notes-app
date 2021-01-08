2047f398501476c4a274522b16890e1aa88ccc5d
Eliel M. Gaspar <elielmichel@gmail.com>
2021-01-08 20:46:22 -0300

    Aula 11

--- src/App.js
+++ src/App.js
@@ -8,18 +8,26 @@ class App extends Component {
   constructor() {
     super();
     this.notas = [];
+    this.state = {
+      notas:[]
+    };
   }
 
   criarNota(titulo, texto) {
     const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas: novoArrayNotas,
+    }
     this.notas.push(novaNota);
+    this.setState(novoEstado)
   }
 
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)} />
-        <ListaDeNotas notas={this.notas} />
+        <ListaDeNotas notas={this.state.notas} />
       </section>
     );
   }
