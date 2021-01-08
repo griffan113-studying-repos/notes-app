077480fbcccf9a3211e54827628f7746543c7f07
Eliel M. Gaspar <elielmichel@gmail.com>
2021-01-08 18:12:31 -0300

    Aula 7

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import "./estilo.css";
+
 class FormularioCadastro extends Component {
+  constructor() {
+    super(); 
+    this.titulo = "";
+  }
+
+  handleMudançaTitulo(evento) {
+    this.titulo = evento.target.value;
+  }
+
   render() {
     return (
-      <form className="form-cadastro ">
+      <form className="form-cadastro">
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
+          onChange={this.handleMudançaTitulo}
         />
         <textarea
           rows={15}
